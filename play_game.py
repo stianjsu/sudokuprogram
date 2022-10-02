c6d090a429a83ab32c12a67cf6404e02ee9ee236
stianjsu <stianjsu@gmail.com>
2022-10-02 16:29:19 +0200

    Added gitignore, updated readme

--- play_game.py
+++ play_game.py
@@ -88,11 +88,11 @@ def find_solution(table):
         return False
 
     solve_board(0,0)
-    print(time.perf_counter()-time1)
+    print(f"Alg Solve Time: {time.perf_counter()-time1}")
     return table
 
 def save_board_to_file():
-    f = open(input("Type filename where you want to save: (board.txt)"), "w")
+    f = open(input("Type filename where you want to save: (board.txt)\n>"), "w")
 
     for i in range(len(game_board)-1):
         write_line = ""
@@ -181,11 +181,6 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 X = 810 + 44 + 300
 Y = 810 + 44
 
-display_surface = pygame.display.set_mode((X, Y ))
-pygame.display.set_caption('Sudoku')
-display_surface.fill(black)
-
-
 startbuttons = {
     "spmbox": button(350, 284, 500, 90, white, white, "Do you have a pre-made board?"),
     "yesbox": button(375, 450, 200, 90, white, lightblue, "Yes"),
@@ -196,8 +191,11 @@ startbuttons = {
 }
 
 
-answered = False
+display_surface = pygame.display.set_mode((X, Y ))
+pygame.display.set_caption('Sudoku')
+display_surface.fill(black)
 
+answered = False
 
 while not answered:
     display_surface.fill(black)
@@ -274,7 +272,6 @@ for i in board:
     game_board.append(i[:])
 game_boardClass = assign_sudoku_class(game_board)
 
-print(game_boardClass)
 
 main_buttons = {
     "save_board":  button(895,   Y//5, 230, 90, white, lightblue, "Save board to file", 24),
@@ -377,80 +374,3 @@ while run:
 
 
 pygame.quit()
-
-
-
-'''
-print("Quit game without saving by typing command: 'quit'")
-print("Delete number by typing command: 'delete'")
-print("Show solution by typing command: 'solution'")
-print("Reset board in this session by typing: 'reset'")
-print("Save to file board by typing: 'save board'")
-run = True
-while run:
-    print_table(game_board)
-    inp = input("Place number: row(1-9), col(1,9), nr(1-9)")
-    try:
-        if inp.lower() == "quit":
-            run = False
-
-        elif inp.lower() == "reset":
-            game_board = []
-            for i in board:
-                game_board.append(i[:])
-
-        elif inp.lower() == "solution":
-            run = False
-            print_table(find_solution(game_board))
-
-        elif inp.lower() == "delete":
-            deletenumb = input("Delete number: row(1-9), col(1,9)")
-            deletenumb = deletenumb.replace(" ", "").split(",")
-            deletepos = []
-            for t in deletenumb:
-                deletepos.append(int(t))
-
-            game_board[deletepos[0]-1][deletepos[1]-1] = 0
-
-        elif inp.lower() == "save board":
-            f = open(input("Type filename where you want to save: (board.txt)"), "w")
-
-            for i in range(len(game_board)-1):
-                write_line = ""
-                for t in range(len(game_board[i])-1):
-                    write_line += f"{game_board[i][t]}, "
-                write_line += f"{game_board[i][len(game_board[i])-1]}\n"
-                f.write(write_line)
-            write_line = ""
-            for u in range(len(game_board[8])-1):
-                write_line += f"{game_board[8][u]}, "
-            write_line += f"{game_board[8][len(game_board[8]) - 1]}"
-            f.write(write_line)
-            f.close()
-            rune = False
-
-        else:
-            inp = inp.replace(" ", "").split(",")
-            guess = []
-            try:
-                for t in inp:
-                    guess.append(int(t))
-
-                if is_legal_move(guess[1]-1, guess[0]-1, guess[2], game_board) and \
-                        in_unsolved_spaces(guess[1]-1, guess[0]-1, unsolved_spaces):
-
-                    game_board[guess[0]-1][guess[1]-1] = guess[2]
-                else:
-                    print("Illegal move, try again")
-
-            except:
-                print("Write numbers between 1 and 9")
-
-            if len(find_unsolved_spaces(game_board)) == 0:
-                run = False
-                print("You did it! So proud of you :D")
-
-    except:
-        print("Unknown input. Try again")
-
-'''
